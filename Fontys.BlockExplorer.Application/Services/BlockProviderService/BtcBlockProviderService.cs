f3852514a7aa883d89a60d80286e94500eb05532
Bram Schulpen <bram.schulpen@live.nl>
2022-09-11 10:44:18 +0200

    btc provider

---
+++ Fontys.BlockExplorer.Application/Services/BlockProviderService/BtcBlockProviderService.cs
@@ -0,0 +1,12 @@
+﻿using Fontys.BlockExplorer.Domain.Models;
+
+namespace Fontys.BlockExplorer.Application.Services.BlockProviderService
+{
+    public class BtcBlockProviderService : IBlockProviderService
+    {
+        public Task<Block> GetBlockAsync(string hash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Fontys.BlockExplorer.Application/Services/BlockProviderService/IBlockProviderService.cs
@@ -0,0 +1,9 @@
+﻿using Fontys.BlockExplorer.Domain.Models;
+
+namespace Fontys.BlockExplorer.Application.Services.BlockProviderService
+{
+    public interface IBlockProviderService
+    {
+        Task<Block> GetBlockAsync(string hash);
+    }
+}
