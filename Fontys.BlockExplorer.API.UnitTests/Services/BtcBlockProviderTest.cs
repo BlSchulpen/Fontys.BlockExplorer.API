803f367740697908cd3c8d0ef8ebd30c28baec53
Bram Schulpen <bram.schulpen@live.nl>
2022-09-24 11:50:32 +0200

    small update

--- Fontys.BlockExplorer.API.UnitTests/Services/BtcBlockProviderTest.cs
+++ Fontys.BlockExplorer.API.UnitTests/Services/BtcBlockProviderTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Configs;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -8,6 +9,7 @@ using Fontys.BlockExplorer.API.Dto.Response;
 using Fontys.BlockExplorer.API.UnitTests.Factories;
 using Fontys.BlockExplorer.Application.Services.BlockProviderService;
 using Fontys.BlockExplorer.Domain.CoinResponseModels.BtcCore.Block;
+using Fontys.BlockExplorer.Domain.Enums;
 using Fontys.BlockExplorer.Domain.Models;
 using Fontys.BlockExplorer.NodeWarehouse.NodeServices;
 using Moq;
@@ -45,7 +47,7 @@ namespace Fontys.BlockExplorer.API.UnitTests.Services
             var service = new BtcBlockProviderService(mockNodeService.Object,mapper);
 
             //act
-            var result = service.GetBlockAsync(blockNr.ToString());
+            var result = await service.GetBlockAsync(blockNr.ToString());
 
             //assert
             mapper.Should().NotBeNull();
@@ -55,16 +57,30 @@ namespace Fontys.BlockExplorer.API.UnitTests.Services
         {
             const int nrInputs = 3;
             const int nrOutputs = 3;
-            const int nrTransactions = 5;
+            const int nrTransactions = 1; //TODO Find a way to give unique mock responses for different parameters
             var btcBlockResponse = _blockFactory.BlockResponse(blockNr, nrTransactions);
             var mockNodeService = new Mock<IBtcNodeService>();
             mockNodeService.Setup(nodeService => nodeService.GetBlockFromHashAsync(blockNr.ToString())).ReturnsAsync(btcBlockResponse);
             for (var i = 0; i < nrTransactions; i++)
             {
                 var transactionResponse = _transactionFactory.BtcTransactionResponse(nrInputs,nrOutputs,i);
-                mockNodeService.Setup(nodeService => nodeService.GetRawTransactionAsync(i.ToString())).ReturnsAsync(transactionResponse);
+                var test = i.ToString();
+                mockNodeService.Setup(nodeService => nodeService.GetRawTransactionAsync(test)).ReturnsAsync(transactionResponse);
             }
+
             return mockNodeService;
+        }
+
+        private MapperConfiguration BlockMapperConfig()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<BlockResponse, Block>()
+                .ForMember(dest => dest.Transactions, act => act.MapFrom(src => src.Transactions))
+                .ForMember(dest => dest.CoinType, act => act.MapFrom(src => CoinType.BTC))
+                .ForMember(dest => dest.NetworkType, act => act.MapFrom(src => NetworkType.BtcMainet))
+            
+            );
+            
+            return config;
         }
     }
 }
