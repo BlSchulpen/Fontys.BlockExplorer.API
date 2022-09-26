57e9198e3ea43feffe0d37b1e2e5fc44ca90e073
Bram Schulpen <bram.schulpen@live.nl>
2022-09-26 14:01:33 +0200

    Remove bad blocks tests (WIP)

--- Fontys.BlockExplorer.API.UnitTests/Services/NodeMonitoringServiceTest.cs
+++ Fontys.BlockExplorer.API.UnitTests/Services/NodeMonitoringServiceTest.cs
@@ -50,7 +50,7 @@ namespace Fontys.BlockExplorer.API.UnitTests.Services
             //arrange
             const int nrStored = 3;
             const int latestBlock = nrStored - 1;
-            var stored = GetStoredBlocks(nrStored);
+            var stored = GetChainBlocks(nrStored);
             _blockDataProviderServiceMock.Setup(b => b.GetHashFromHeightAsync(latestBlock)).ReturnsAsync(latestBlock.ToString());
             _blockDataProviderServiceMock.Setup(b => b.GetBlockAsync(latestBlock.ToString())).ReturnsAsync(stored.FirstOrDefault(b => b.Hash == latestBlock.ToString()));
             _dbContextMock.Setup(context => context.Blocks).ReturnsDbSet(stored);
@@ -70,30 +70,21 @@ namespace Fontys.BlockExplorer.API.UnitTests.Services
         public async Task RemoveBadBlock_BadStored_ReturnsBadBlocks()
         {
             //arrange
-            const int nrStored = 3;
+            const int nrChainBlocks = 3;
             const int nrBadStored = 2;
-            var stored = GetStoredBlocks(nrStored);
-            var badBlocks = GetBadBlocks(nrBadStored, nrStored);
-            badBlocks.AddRange(stored);
-            var latestBlock = badBlocks.Count() - 1;
+            var chainBlocks = GetChainBlocks(nrChainBlocks);
+            var storedBlocks = new List<Block>(chainBlocks);
+            storedBlocks.RemoveRange((nrChainBlocks - nrBadStored), nrBadStored);
+            var badBlocks = GetBadBlocks(nrBadStored, nrChainBlocks);
+            storedBlocks.AddRange(badBlocks);
 
+            _dbContextMock.Setup(b => b.Blocks).ReturnsDbSet(storedBlocks);
+
+            var latestBlock = storedBlocks.Count() - 1;
             _blockDataProviderServiceMock.Setup(b => b.GetHashFromHeightAsync(latestBlock)).ReturnsAsync(latestBlock.ToString());
-            //            _blockDataProviderServiceMock.Setup(b => b.GetBlockAsync(latestBlock.ToString())).ReturnsAsync(stored.FirstOrDefault(b => b.Hash == latestBlock.ToString()));
-             
-            //tests
 
 
-
-            _blockDataProviderServiceMock.Setup(b => b.GetBlockAsync(latestBlock.ToString())).ReturnsAsync(stored.FirstOrDefault(b => b.Hash == latestBlock.ToString()));
-            _blockDataProviderServiceMock.Setup(b => b.GetBlockAsync(latestBlock.ToString())).ReturnsAsync(stored.FirstOrDefault(b => b.Hash == latestBlock.ToString()));
-
-            var test = 
-
-
-
-
-
-            _dbContextMock.Setup(context => context.Blocks).ReturnsDbSet(badBlocks);
+            UpdateBlockProvider(chainBlocks);
             var mockAddressRestoreService = new Mock<IAddressRestoreService>();
             var service = new NodeMonitoringService(_dbContextMock.Object, _blockDataProviderResolverMock.Object, mockAddressRestoreService.Object);
             const CoinType coinType = CoinType.BTC;
@@ -105,12 +96,22 @@ namespace Fontys.BlockExplorer.API.UnitTests.Services
             removedBlocks.Should().HaveCount(badBlocks.Count); //TODO like ipv count
         }
 
+        private void UpdateBlockProvider(List<Block> chainBlocks)
+        {
+            var latestHash = chainBlocks.FirstOrDefault(x => x.Height == chainBlocks.Max(b => b.Height)).Hash;
+            _blockDataProviderServiceMock.Setup(b => b.GetBestBlockHashAsync()).ReturnsAsync(latestHash);
+            foreach (var block in chainBlocks)
+            {
+                _blockDataProviderServiceMock.Setup(b => b.GetBlockAsync(block.Hash)).ReturnsAsync(block);
+                _blockDataProviderServiceMock.Setup(b => b.GetHashFromHeightAsync(block.Height)).ReturnsAsync(block.Hash);
+            }
+        }
 
 
         private List<Block> GetBadBlocks(int nrBadBlocks, int nrBlocks)
         {
             var blocks = new List<Block>();
-            for (var i = nrBlocks ; i < (nrBadBlocks + nrBlocks) ; i++)
+            for (var i = (nrBlocks - nrBadBlocks); i < nrBlocks ; i++)
             {
                 var badBlock = new Block() { Hash = "X" + i, Height = i, NetworkType = NetworkType.BtcMainet, CoinType = CoinType.BTC, PreviousBlockHash = "X" + (i -1) };
                 blocks.Add(badBlock);
@@ -118,7 +119,7 @@ namespace Fontys.BlockExplorer.API.UnitTests.Services
             return blocks;
         }
 
-        private List<Block> GetStoredBlocks(int nrBlocks)
+        private List<Block> GetChainBlocks(int nrBlocks)
         {
             var storedBlocks = new List<Block>();
             for (var i = 0; i < nrBlocks; i++)
